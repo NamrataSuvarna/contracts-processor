982a1aeb1b5055f92f297565f2ae19bf17c87e94
NamrataSuvarnaCherodian <78306435+NamrataSuvarnaCherodian@users.noreply.github.com>
2021-10-13 17:43:02 +0530

    Create ContractsProcessor.java

---
+++ src/main/java/home/contracts.processor/ContractsProcessor.java
@@ -0,0 +1,32 @@
+package home.contracts.processor;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import home.contracts.processor.model.Contract;
+import home.contracts.processor.util.Utility;
+
+public class ContractsProcessor {
+
+	private static Map<String, Double> computeFinalPriceShares(List<Contract> contracts) {
+		Map<String, Double> finalContractPriceShares = new HashMap<>();
+		for (Contract contract : contracts) {
+			if (finalContractPriceShares.containsKey(contract.getBusinessUnit())) {
+				double actualPriceShare = finalContractPriceShares.get(contract.getBusinessUnit())
+						+ contract.getActualPriceShare();
+				finalContractPriceShares.put(contract.getBusinessUnit(), actualPriceShare);
+			} else {
+				finalContractPriceShares.put(contract.getBusinessUnit(), contract.getActualPriceShare());
+			}
+		}
+		return finalContractPriceShares;
+	}
+
+	public static void main(String[] args) throws IOException {
+		System.out.println(computeFinalPriceShares(Utility
+				.getContracts("fileName", "cutOffDate"))); //TODO Update values
+	}
+
+}
